3e0b5550e52c5c57bc11d384033e681994c4f9e3
Felipe Santos <felipefss@gmail.com>
2017-03-18 14:53:20 +0100

    Implement file metadata microservice.
    - Add index.html
    - Add upload endpoint

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const multer = require('multer');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const upload = multer({ dest: 'uploads/' });
+
+app.use(express.static('public'));
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send({ size: req.file.size });
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
